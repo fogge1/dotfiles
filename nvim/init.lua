594d53c9bb68148107ab7c1c812d8b62f1d634b9
Isak <isak.fogge1@gmail.com>
2026-02-28 01:33:12 +0100

    feat: change split navigation binds

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,11 @@
 vim.g.mapleader = " "
 vim.opt.termguicolors = true
+
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
