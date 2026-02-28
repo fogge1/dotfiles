8425cf545e7ca04c7bcc95f24d511ae656b1be29
Isak <isak.fogge1@gmail.com>
2026-02-28 13:05:09 +0100

    feat: nvim linenumber & key binds

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,12 @@
 vim.g.mapleader = " "
 vim.opt.termguicolors = true
 
+vim.wo.number = true
+vim.api.nvim_set_hl(0, 'LineNr', { fg='white', bold=true })
+
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+vim.keymap.set("n", "<leader>tt", "<cmd>split | terminal<cr>")
+
 vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
